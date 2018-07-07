d350045416037743ec0ee792148bfbf5217d8ed1
pedro_machado <pedro.m@sundance.com>
2018-07-07 19:40:27 +0100

    update

--- 01/conv.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-echo "Resizing images to 40x40"
-mkdir -p ../objects/01
-for I in {100..199}
-do
-	convert "pcd0"$I"r.png"  -resize 40x40\! "../objects/01/"$I".png"
-	convert  "../objects/01/"$I".png" -colorspace Gray -separate -average "../objects/"$I".png"
-done
- 
