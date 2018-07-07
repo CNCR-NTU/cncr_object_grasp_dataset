7fd2c68d5b9f8b1dffd928e89c1db3a632cd9034
pedrombmachado <pedro.baptistamachado@ntu.ac.uk>
2018-07-07 16:21:21 +0100

    First Commit

--- 01/conv.sh
+++ 01/conv.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 echo "Resizing images to 40x40"
+mkdir -p ../objects/01
 for I in {100..199}
 do
-	convert "pcd0"$I"r.png"  -resize 40x40\! "../objects/"$I".png"
-	convert  "../objects/"$I".png" -colorspace Gray -separate -average "../objects/"$I".png"
+	convert "pcd0"$I"r.png"  -resize 40x40\! "../objects/01/"$I".png"
+	convert  "../objects/01/"$I".png" -colorspace Gray -separate -average "../objects/"$I".png"
 done
  
