e41e09de2fbea6b3afd35a563ac982f31487ea05
pedrombmachado <pedro.baptistamachado@ntu.ac.uk>
2018-07-10 12:08:01 +0100

    update

--- scripts/conv.sh
+++ scripts/conv.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 echo "Resizing images to 40x40"
-mkdir -p ../objects
+type=test
+mkdir -p "../"$type
 for I in {1..885}
 do
-	convert "../rawImages/img"$I".png"  -resize 40x40\! "../objects/img"$I".png"
-	convert "../objects/img"$I".png" -colorspace Gray -separate -average "../objects/img"$I".png"
+	convert "../rawImages/img"$I".png"  -resize 40x40\! "../"$type"/img"$I".png"
+	convert "../"$type"/img"$I".png" -colorspace Gray -separate -average "../"$type"/img"$I".png"
 done
 
  
